062bc27bc74f1b8ea77f4581f0d5f5f6a3a68a6e
Joaquin Cernik <joaquincernik@gmail.com>
2021-10-18 21:03:28 -0300

    Last commit

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.get("/", (req, res)=>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
 })
 
+app.get("/register", (req, res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/register.html"))
+})
+
+app.get("/login", (req, res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/login.html"))
+})
 
 app.listen(4000,()=>{
     console.log("Servidor escuchando")  
